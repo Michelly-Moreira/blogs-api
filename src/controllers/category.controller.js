192b9f4f90965b3b25b4f8372d01813ac02c0be1
Michelly Moreira <michelly.daiana@hotmail.com>
2023-04-12 16:40:08 -0300

    correct task 8

--- src/controllers/category.controller.js
+++ src/controllers/category.controller.js
@@ -2,10 +2,10 @@ const { categoryService } = require('../services');
 
 const createCategory = async (req, res) => {
     const { name } = req.body;
-const newCategory = await categoryService.createCategory(name);
-if (!name) return res.status(400).json('"name" is required');
+    if (!name) return res.status(400).json({ message: '"name" is required' });
+    const newCategory = await categoryService.createCategory(name);
 
-return res.status(201).json(newCategory);
+    return res.status(201).json(newCategory);
 };
 
 module.exports = {
